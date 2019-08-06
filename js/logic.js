8c1971cc165b77f585397e0b2cb45574720394dd
Nick Forma <nickforma60@gmail.com>
2019-08-06 19:40:58 -0400

    add button generated

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   //spongebob characters
   let topics = [
     "Spongebob Squarepants",
@@ -9,59 +10,68 @@ $(document).ready(function() {
     "Plankton"
   ];
 
-  topics.forEach(function(e) {
-    console.log(e);
-    $(".button-section").append(
-      `<button class="gif-btn">${e}</button>
-            `
-    );
-  });
+  //making buttons
+  const buildButtons = function(){
+    $('.button-section').html('');
+
+    topics.forEach(function(e) {
+      console.log(e);
+      $(".button-section").append(
+        `<button class="gif-btn">${e}</button>
+        `
+        );
+      });
+    }
 
   $(".button-section").on("click", ".gif-btn", function(e) {
-    let term = e.currentTarget.innerText
+    let term = e.currentTarget.innerText;
     console.log(term);
-    gifSearch(term)
+    gifSearch(term);
   });
 
   const gifSearch = function(term) {
     let API = "J5Lbvrnv1kH2UiPQyYqRPk7KUEiiTc6K";
-    let url =
-      `https://api.giphy.com/v1/gifs/search?api_key=${API}&q=${term}&limit=10`;
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${API}&q=${term}&limit=10`;
 
     $.ajax({
       method: "GET",
       url: url
     }).then(function(response) {
-      writeMemes(response)
-      
+      writeMemes(response);
     });
-
   };
-    const writeMemes = function(data){
-        data.data.forEach(function(image){
-            console.log(image.images.fixed_height.url)
-            
-            let imageURL = image.images.fixed_height.url
-            $('.image-display').append(
-                `<img src="${imageURL}">
+
+  buildButtons();
+
+  const writeMemes = function(data) {
+    data.data.forEach(function(image) {
+      console.log(image.images.fixed_height.url);
+
+      let imageURL = image.images.fixed_height.url;
+      $(".image-display").prepend(
+        `<img src="${imageURL}">
                 `
-            )
-        })
-
-    }
-
-    $('#clear-btn').on('click', function(e){
-      e.preventDefault();
-      $('.image-display').empty()
-    })
-
-$('#search-btn').on('click', function(event){
-  event.preventDefault();
-  console.log(event)
-})
-
-
-
-
-
+      );
     });
+  };
+
+  
+  $("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    let term = $('.search').val()
+    console.log(term);
+    topics.push(term)
+    buildButtons();
+  });
+
+
+
+
+  $("#clear-btn").on("click", function(e) {
+    e.preventDefault();
+    $(".image-display").empty();
+  });
+
+
+
+});
