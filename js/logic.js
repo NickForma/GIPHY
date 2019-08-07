60d8c729313ff06063d6ebdae7c13002e65cf745
Nick Forma <nickforma60@gmail.com>
2019-08-06 20:37:23 -0400

    animate and still features now clickable

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   //spongebob characters
   let topics = [
     "Spongebob Squarepants",
@@ -11,21 +10,23 @@ $(document).ready(function() {
   ];
 
   //making buttons
-  const buildButtons = function(){
-    $('.button-section').html('');
+  const buildButtons = function() {
+    $(".button-section").html("");
+    $(".image-display").html("");
 
     topics.forEach(function(e) {
       console.log(e);
       $(".button-section").append(
         `<button class="gif-btn">${e}</button>
         `
-        );
-      });
-    }
+      );
+    });
+  };
 
   $(".button-section").on("click", ".gif-btn", function(e) {
     let term = e.currentTarget.innerText;
     console.log(term);
+    $(".image-display").html("");
     gifSearch(term);
   });
 
@@ -38,6 +39,7 @@ $(document).ready(function() {
       url: url
     }).then(function(response) {
       writeMemes(response);
+      console.log(response)
     });
   };
 
@@ -45,33 +47,48 @@ $(document).ready(function() {
 
   const writeMemes = function(data) {
     data.data.forEach(function(image) {
-      console.log(image.images.fixed_height.url);
-
+      //animated and still url
       let imageURL = image.images.fixed_height.url;
+      let url = image.images.fixed_height_still.url;
+
       $(".image-display").prepend(
-        `<img src="${imageURL}">
+        `
+          <img class="gif" src="${url}" data-still=${url} data-animate=${imageURL} data-state="still">
+        
                 `
       );
     });
   };
 
-  
   $("#search-btn").on("click", function(event) {
     event.preventDefault();
-    let term = $('.search').val()
+    let term = $(".search").val();
     console.log(term);
-    topics.push(term)
+    topics.push(term);
     buildButtons();
   });
-
-
-
 
   $("#clear-btn").on("click", function(e) {
     e.preventDefault();
     $(".image-display").empty();
   });
 
+$('.image-display').on('click', '.gif', function(){
+    let state = $(this).attr('data-state')
+    console.log(this);
 
+    if (state === 'still') {
+      $(this).attr({
+        src: this.dataset.animate,
+        'data-state': 'animated'
+      })
+    }else {
+      $(this).attr({
+        src: this.dataset.still,
+        'data-state': 'still'
+      })
+    }
+
+})
 
 });
